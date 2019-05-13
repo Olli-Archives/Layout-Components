c45d408cd9410e810bfc5c624a05ecc942c58fe2
ollinieminen <otn987@gmail.com>
2019-05-13 13:15:03 -0700

    push

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hi');
+import styles from './index.css';
+
+//append a ul
+//and append a li with spot rover bing and joe
+const root = document.getElementById('root');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+console.log(root);
+root.appendChild(ul);
+
+li.textContent = 'skip';
+ul.appendChild(li);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 
-// eslint-disable-next line
+// eslint-disable-next-line
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -11,7 +11,8 @@ module.exports = {
     port: 7890
   },
   plugins: [
-    new HtmlPlugin({ template: './src/index.html' })
+    new HtmlPlugin({ template: './src/index.html' }),
+    new CleanPlugin()
   ],
   module: {
     rules: [
