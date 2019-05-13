fa69eb7f36fcfbfb3c02703517b53bdfccb94e08
ollinieminen <otn987@gmail.com>
2019-05-13 15:07:08 -0700

    simple dog done

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// src/components/App.js
+import React from 'react';
+import Dog from './Dog';
+import Header from './Header';
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Dog/>
+    </div>
+  );
+}
